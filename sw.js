04e82d872e09da081384fd0be44270f887561d53
bby-ye <ygf2704646910@qq.com>
2026-04-08 20:04:18 +0800

    Site updated: 2026-04-08 20:04:17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1775649665938";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1775649854946";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
